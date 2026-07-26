3433fe361772fa67565e59f71f2e41be48519470
phantom-dot-dev <phantom.opera.dev@gmail.com>
2026-07-26 00:32:25 -0400

    lifetime with string slices

--- apps/traits-101/src/lifetime_for_ref_validation.rs
+++ apps/traits-101/src/lifetime_for_ref_validation.rs
@@ -10,11 +10,14 @@ pub fn init_lifetime_for_ref_validaiton() {
 // }
 
 fn sample_string_return(s1: &str) -> &str {
-    "abc"
+    // "abc" // works. as string literal has static lifetime, it stays in the memory as long as the program is running ("abc" is embedded into the binary)
+    // when the program runs, it's binary is loaded into ram memory from hard disk,  and the returned address `&str` pointer will be valid until the variable holding that goes out of scope
+    let sth = "abc";
+    sth
 }
 
-// the code will not compile, as the local variable is cleaned up after the function's scope ends, only variable defined in the parameter will survive and passed through the returned &str
 // also note, when we're returning a borrowed reference, its actually the pointer to a memory address, not the actual value
+// the code will not compile, as the local variable is cleaned up after the function's scope ends, only variable defined in the parameter will survive and passed through the returned &str
 // fn sample_string_return(s1: &str) -> &str {
 //     let x = format!("{} {}", "abc", s1);
 //     &x
